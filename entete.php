dec8eec2f508490770f7aea93f39126f6128ef99
Niels <niels.deliens3@gmail.com>
2025-11-04 15:24:35 +0100

    Commit CGI V4

--- entete.php
+++ entete.php
@@ -20,3 +20,12 @@ if (isset($_SESSION['login'])) {
     echo '<a href="formulaire.php" style="float:right;">Connexion</a>';
 }
 ?>
+
+<?php
+if (isset($_SESSION['login'])) {
+    echo "<p>Connecté en tant que " . $_SESSION['login'] . "</p>";
+    echo '<a href="logout.php" style="float:right;">Déconnexion</a>';
+} else {
+    echo '<a href="formulaire.php" style="float:right;">Connexion</a>';
+}
+?>
