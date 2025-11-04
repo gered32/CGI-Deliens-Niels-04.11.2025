c6eafad476894f573855bb2c69e89ef2598b2dd0
Niels <niels.deliens3@gmail.com>
2025-11-04 15:20:59 +0100

    Commit CGI Style V3

--- entete.php
+++ entete.php
@@ -2,10 +2,10 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CGI TP4 - Niels Deliens</title>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <header>
